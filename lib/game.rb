0312081691dbf861b157f25dc04582fce7900b09
SachaPalayret <55750049+SachaPalayret@users.noreply.github.com>
2019-10-17 17:20:06 +0200

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -50,6 +50,17 @@ end
 
     # Méth permettant d'obtenir le nom des joueurs, on utilise 'gest.chomp'
 def get_names 
+    puts "Les joueurs inscrivent tour à tour leur symbole sur une grille\
+qui n'a pas de limites ou qui n'a que celles du papier sur lequel on joue.\
+Le premier qui parvient à aligner cinq de ses symboles horizontalement,\
+verticalement ou en diagonale gagne un point.
+
+Le morpion donne un avantage assez important à celui qui commence.\
+Des formes évoluées existent, comme le Gomoku ou la Pente,\
+qui ajoutent à la notion d'alignement une notion de prise.\
+Le renju prévoit des handicaps pour le joueur qui commence,\
+ce qui permet d'équilibrer les chances des deux joueurs.\
+Une partie dure environ une minute. "
     puts "Player X name : "
     name1 = gets.chomp
     puts "Player O name : "
